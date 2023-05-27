b8abc54b87bd67016439dc370afd00618728be7a
spinfast319 <spinfast319@protonmail.com>
2023-05-26 23:21:03 -0400

    started extraction of common functions into origin script library

--- Origin-Write-Metadata.py
+++ Origin-Write-Metadata.py
@@ -21,6 +21,8 @@ import shutil  # Imports functionality that lets you copy files and directory
 import datetime  # Imports functionality that lets you make timestamps
 import mutagen  # Imports functionality to get metadata from music files
 
+import origin_script_library as osl  # Imports common code used across all origin scripts
+
 #  Set your directories here
 album_directory = "M:\PROCESS"  # Which directory do you want to start with?
 log_directory = "M:\PROCESS-LOGS\Logs"  # Which directory do you want the log in?
@@ -484,9 +486,8 @@ def main():
         print("I will shred this universe down to its last atom and then, with the stones you've collected for me, create a new one...")
         print("")
 
-        # Get all the subdirectories of album_directory recursively and store them in a list:
-        directories = [os.path.abspath(x[0]) for x in os.walk(album_directory)]
-        directories.remove(os.path.abspath(album_directory))  # If you don't want your main directory included
+        # Get all the subdirectories of album_directory recursively and store them in a list
+        directories = osl.set_directory(album_directory)
 
         #  Run a loop that goes into each directory identified in the list and runs the function that sorts the folders
         for i in directories:
